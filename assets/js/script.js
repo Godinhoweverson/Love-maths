ada88455653e523d45d6ba7a9e7a109d54ac1718
Godinhoweverson <>
2023-05-08 21:02:48 +0000

    Add DOM content loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-function runGame(){
+// wait for the dom to finish loading before running the game
+// get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function{
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You Click submit!");
+            } else {
+                let gametype = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
