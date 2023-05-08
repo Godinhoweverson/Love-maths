a62018c34928cf76f1937525cd1e299db4fbe29e
Godinhoweverson <>
2023-05-08 20:30:33 +0000

    set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswe() {
+
+}
+
+function displayAddtionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
